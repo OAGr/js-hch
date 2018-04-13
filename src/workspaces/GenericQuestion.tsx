1728f71c95da2a1e8f5f2871b89ce5c881eef257
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-04-12 23:49:51 -0700

    Refactored GenericQuestion a bit

--- src/workspaces/GenericQuestion.tsx
+++ src/workspaces/GenericQuestion.tsx
@@ -2,31 +2,34 @@ import { Workspace } from './Workspace';
 import { FindStrategy } from './FindStrategy';
 import { ApplyStrategyToQuestion } from './ApplyStrategyToQuestion';
 
+const moonWeigh = ({ question, instance }: any) => {
+    if (question !== 'How much does the moon weigh?') { return false; }
+    const child = new FindStrategy();
+    const strategy = child.execute({ question });
+    const toChild = new ApplyStrategyToQuestion();
+    const response = toChild.execute({
+        strategy: strategy.value,
+        question: question
+    });
+    return { ...response, instance, children: [strategy.instance, response.instance] };
+};
+
+const timeIsIt = ({ question, instance }: any) => {
+    if (question !== 'What time is it?') { return false; }
+    return {
+        value: '3:35',
+    };
+};
+
 export class GenericQuestion extends Workspace {
     _run() {
-        const {question} = this.props;
-        switch (question) {
-            case 'What time is it?':
-                return {
-                    value: '3:35',
-                };
-            case 'How much does the moon weigh?':
-                const child = new FindStrategy();
-                const strategy = child.execute({question});
-                const toChild = new ApplyStrategyToQuestion();
-                const response = toChild.execute({
-                    strategy: strategy.value,
-                    question: question
-                });
-                return {...response, instance: this, children: [strategy.instance, response.instance]};
-            case 'Apply strategy (Try multiplying the volume of the moon by its density.) to question (How much does the moon weigh?)':
-                return {
-                    value: '34'
-                };
-            default:
-                return {
-                    error: this.invalidInputError(this.props.question)
-                };
-        } 
+        const { question } = this.props;
+        const instance = this;
+        const params = { question, instance };
+        return moonWeigh(params) ||
+            timeIsIt(params) ||
+            {
+                error: this.invalidInputError(this.props.question)
+            };
     }
 }
